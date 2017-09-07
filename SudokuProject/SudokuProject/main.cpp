39b79712dd80887ea66ab447d4b2071527dd29de
wzsfty <y1159821460@gmail.com>
2017-09-07 21:43:38 +0800

    init

---
+++ SudokuProject/SudokuProject/main.cpp
@@ -0,0 +1,23 @@
+
+
+
+/* 
+
+#include "stdio.h"
+#include "stdlib.h"
+
+void main()
+{
+	printf("hello world");
+	system("pause");
+}
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+}
