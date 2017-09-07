0b1b56e8ece7dfcb655526ee4c7b137b72fa9c34
wzsfty <y1159821460@gmail.com>
2017-09-07 22:37:23 +0800

    采用dfs+回溯

--- SudokuProject/SudokuProject/main.cpp
+++ SudokuProject/SudokuProject/main.cpp
@@ -14,10 +14,53 @@ void main()
 
 */
 
-#include<iostream>
-using namespace std;
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
 
+int dfs(int x, int y);
+bool judge(int i, int y, int num);
+int a[9][9];
 int main()
 {
+	int i, j;
+	a[9][9] = {
+		{4,0,0,0,8,0,0,0,0},
+		{0,2,7,0,0,0,3,4,0},
+		{8,9,0,4,0,7,0,1,5},
+		{6,0,0,2,0,5,0,0,9},
+		{0,8,0,0,0,0,0,3,0},
+		{2,0,0,8,0,9,0,0,4},
+		{1,4,0,7,0,6,0,9,3},
+		{0,6,5,0,0,0,2,7,0},
+		{0,0,0,0,9,0,0,0,0}
+	};
+	dfs(0, 0);
+	return 0;
+}
+
+
+int dfs(int x, int y)
+{
+	int i;
+	if (x == 9)
+	{
+		for (i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+				printf("%d", a[i][j]);
+			printf("\n");
+		}
+	}
+
+	if (a[x][y] == 0)
+	{
+		for(i=1;i<=9;i++)
+	}
+
+
 
 }
+
+
+
