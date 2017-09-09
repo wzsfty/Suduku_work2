6187c7b9aa36f94ae9d3b6f224bae78d7fc492b3
wzsfty <y1159821460@gmail.com>
2017-09-09 21:00:47 +0800

    使用回溯法生成数独

--- SudokuProject/SudokuProject/main.cpp
+++ SudokuProject/SudokuProject/main.cpp
@@ -1,107 +1,155 @@
 
 
+/*
+#include <cstdio>
 
-/* 
+#define SUDUKU_Num 2
 
-#include "stdio.h"
-#include "stdlib.h"
-
-void main()
+bool check(int sudoku[][9], int i, int j)
 {
-	printf("hello world");
-	system("pause");
-}
+	int value = sudoku[i][j];
+	int p, q;
+	int m, n;
 
-*/
+	//检测列中有没有重复
+	for (p = 0; p < 9; p++)
+		if (p != i && sudoku[p][j] == value)
+			return false;
+	//行
+	for (p = 0; p < 9; p++)
+		if (p != j && sudoku[i][p] == value)
+			return false;
 
-#include <string.h>
-#include <math.h>
-#include <stdio.h>
-#include "stdlib.h"
-
-
-int dfs(int x, int y);
-bool judge(int i, int y, int num);
-int a[9][9]= { 
-{ 4,0,0,0,8,0,0,0,0 },
-{ 0,2,7,0,0,0,3,4,0 },
-{ 8,9,0,4,0,7,0,1,5 },
-{ 6,0,0,2,0,5,0,0,9 },
-{ 0,8,0,0,0,0,0,3,0 },
-{ 2,0,0,8,0,9,0,0,4 },
-{ 1,4,0,7,0,6,0,9,3 },
-{ 0,6,5,0,0,0,2,7,0 },
-{ 0,0,0,0,9,0,0,0,0 } 
-};
-
-
-int main()
-{
-	dfs(0, 0);
-
-	system("pause");
-	return 0;
-}
-
-
-int dfs(int x, int y)
-{
-	int i;
-	if (x == 9)
-	{
-		
-		
-		for (i = 0; i < 9; i++)
-		{
-			for (int j = 0; j < 9; j++)
-				printf("%d", a[i][j]);
-			printf("\n");
-		}
-		
-		return 0;
-	}
-
-	if (a[x][y] == 0)//填数
-	{
-		for (i = 1; i <= 9; i++)
-		{
-			if (judge(x, y, i))
-			{
-				a[x][y] = i;
-				dfs(x + (y + 1) / 9, (y + 1) % 9);
-			}
-		}
-		a[x][y] = 0;//回溯
-	}
-	else 
-		dfs(x + (y + 1) / 9, (y + 1) % 9);
-	return 0;
-}
-
-bool judge(int i, int j, int num)
-{
-	int x, y, k, t;
-	//找到纵向第几个方块
-	if (i < 3) y = 0;
-	else if (i < 6) y = 1;
-	else y = 2;
-
-	//找到横向第几个方块
-	if (j < 3) x = 0;
-	else if (j < 6) x = 1;
-	else x = 2;
-
-	//判断一行/一列是否有重复
-	for (k = 0; k < 9; k++)
-		if (a[i][k] == num && k != j)  return false;
-	for (k = 0; k < 9; k++)
-		if (a[k][j] == num && k != i) return false;
-	for (k = y * 3; k < y * 3 + 3; k++)
-		for (t = x * 3; t < x * 3 + 3; t++)
-			if (a[k][t] == num && k != i && t != j)
+	p = i / 3;
+	q = j / 3;
+	for (m = p * 3; m < p * 3 + 3; m++)
+		for (n = q*#; n < q * 3 + 3; n++)
+			if (m != i && n != j && sudoku[m][n] == value)
 				return false;
+
 	return true;
 }
 
+void print_sudoku(int sudoku[][9])
+{
+	int i, j;
+	for (i = 0; i < 9; i++)
+		for (j = 0; j < 9; j++)
+			printf("%d", sudoku[i][j]);
+	printf("\n");
+}
 
+int main()
+{
+	int sudoku[9][9] = { 0 };
+	int i, j;
+	for (i = 0; i < 9; i++)
+		for (j = 0; j < 9; j++)
+			sudoku[i][j] = 0;
 
+	return 0;
+}
+*/
+
+#include <stdio.h>  
+#include <time.h>  
+#include <math.h>
+#include <stdlib.h>
+
+/*要生成的数独矩阵个数*/
+#define SUDOKU_NUM 10000
+
+/**
+* 函数is_digital_match
+* 检测sudoku[i][j]上的值是否符合要求
+*/
+int is_digital_match(int sudoku[][9], int i, int j)
+{
+	int temp = sudoku[i][j];
+	int p, q;
+	int m, n;
+
+	for (p = 0; p<9; p++)
+		if (p != i && sudoku[p][j] == temp)
+			return 0;
+	for (p = 0; p<9; p++)
+		if (p != j && sudoku[i][p] == temp)
+			return 0;
+
+	p = i / 3;
+	q = j / 3;
+	for (m = p * 3; m<p * 3 + 3; m++)
+		for (n = q * 3; n<q * 3 + 3; n++)
+			if (m != i && n != j && sudoku[m][n] == temp)
+				return 0;
+
+	return 1;
+}
+
+/*输出数独矩阵*/
+void sudoku_print(int sudoku[][9])
+{
+	int i, j;
+	for (i = 0; i<9; i++)
+	{
+		for (j = 0; j<9; j++)
+			printf("%4d", sudoku[i][j]);
+		printf("\n");
+	}
+}
+
+int main(void)
+{
+	int sudoku[9][9] = { 0 };
+	int i, j, temp = 0;
+	int k = 0, num = 0;
+
+	
+	for (i = 0; i<9; i++)
+		for (j = 0; j<9; j++)
+			sudoku[i][j] = 0;
+	/*通过添加一些随机性，来每次生成不同的数独矩阵*/
+	/*添加更多随机性，则生成的矩阵将更随即*/
+	/* 
+	for (i = 0; i<9; i++)
+	{
+		temp = rand() % 81;
+		sudoku[temp / 9][temp % 9] = i + 1;
+	} 
+	*/
+	sudoku[0][0]=3; 
+	/*回溯法，构造数独矩阵*/
+	while (1)
+	{
+		i = k / 9;
+		j = k % 9;
+
+		while (1)
+		{
+			sudoku[i][j]++;
+			if (sudoku[i][j] == 10)
+			{
+				sudoku[i][j] = 0;
+				--k;
+				break;
+			}
+			else if (is_digital_match(sudoku, i, j) == 1)
+			{
+				++k;
+				break;
+			}
+		}
+
+		if (k == 81)
+		{
+			printf("Proper sudoku matrix %d: \n", ++num);
+			sudoku_print(sudoku);
+			if (num >= SUDOKU_NUM)
+				return 0;
+
+			--k;
+		}
+	}
+	system("pause");
+	return 0;
+}
