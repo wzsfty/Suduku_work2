8382e27f47729a0ad73feeffea982ad06da5ca06
wzsfty <y1159821460@gmail.com>
2017-09-09 23:54:24 +0800

    完成-c 20参数设置

--- SudokuProject/SudokuProject/main.cpp
+++ SudokuProject/SudokuProject/main.cpp
@@ -1,105 +1,25 @@
 
 
-/*
-#include <cstdio>
-
-#define SUDUKU_Num 2
-
-bool check(int sudoku[][9], int i, int j)
-{
-	int value = sudoku[i][j];
-	int p, q;
-	int m, n;
-
-	//检测列中有没有重复
-	for (p = 0; p < 9; p++)
-		if (p != i && sudoku[p][j] == value)
-			return false;
-	//行
-	for (p = 0; p < 9; p++)
-		if (p != j && sudoku[i][p] == value)
-			return false;
-
-	p = i / 3;
-	q = j / 3;
-	for (m = p * 3; m < p * 3 + 3; m++)
-		for (n = q*#; n < q * 3 + 3; n++)
-			if (m != i && n != j && sudoku[m][n] == value)
-				return false;
-
-	return true;
-}
-
-void print_sudoku(int sudoku[][9])
-{
-	int i, j;
-	for (i = 0; i < 9; i++)
-		for (j = 0; j < 9; j++)
-			printf("%d", sudoku[i][j]);
-	printf("\n");
-}
-
-int main()
-{
-	int sudoku[9][9] = { 0 };
-	int i, j;
-	for (i = 0; i < 9; i++)
-		for (j = 0; j < 9; j++)
-			sudoku[i][j] = 0;
-
-	return 0;
-}
-*/
-
 #include <stdio.h>  
 #include <time.h>  
 #include <math.h>
 #include <stdlib.h>
 
-/*要生成的数独矩阵个数*/
-#define SUDOKU_NUM 10000
 
-/**
-* 函数is_digital_match
-* 检测sudoku[i][j]上的值是否符合要求
-*/
-int is_digital_match(int sudoku[][9], int i, int j)
+//检查是否符合数独的约束条件
+bool check(int sudoku[][9], int i, int j);
+
+//输出数独
+void printSudoku(int sudoku[][9]);
+
+
+int main(int argc,char *argv[])
 {
-	int temp = sudoku[i][j];
-	int p, q;
-	int m, n;
-
-	for (p = 0; p<9; p++)
-		if (p != i && sudoku[p][j] == temp)
-			return 0;
-	for (p = 0; p<9; p++)
-		if (p != j && sudoku[i][p] == temp)
-			return 0;
-
-	p = i / 3;
-	q = j / 3;
-	for (m = p * 3; m<p * 3 + 3; m++)
-		for (n = q * 3; n<q * 3 + 3; n++)
-			if (m != i && n != j && sudoku[m][n] == temp)
-				return 0;
-
-	return 1;
-}
-
-/*输出数独矩阵*/
-void sudoku_print(int sudoku[][9])
-{
-	int i, j;
-	for (i = 0; i<9; i++)
-	{
-		for (j = 0; j<9; j++)
-			printf("%4d", sudoku[i][j]);
-		printf("\n");
-	}
-}
-
-int main(void)
-{
+	
+	int T;
+	for(T=0;T<argc;T++)
+	printf("%d",argv[T]);
+	int N = 2;;
 	int sudoku[9][9] = { 0 };
 	int i, j, temp = 0;
 	int k = 0, num = 0;
@@ -108,16 +28,9 @@ int main(void)
 	for (i = 0; i<9; i++)
 		for (j = 0; j<9; j++)
 			sudoku[i][j] = 0;
-	/*通过添加一些随机性，来每次生成不同的数独矩阵*/
-	/*添加更多随机性，则生成的矩阵将更随即*/
-	/* 
-	for (i = 0; i<9; i++)
-	{
-		temp = rand() % 81;
-		sudoku[temp / 9][temp % 9] = i + 1;
-	} 
-	*/
 	sudoku[0][0]=3; 
+	
+	
 	/*回溯法，构造数独矩阵*/
 	while (1)
 	{
@@ -133,7 +46,7 @@ int main(void)
 				--k;
 				break;
 			}
-			else if (is_digital_match(sudoku, i, j) == 1)
+			else if (check(sudoku, i, j) == 1)
 			{
 				++k;
 				break;
@@ -143,8 +56,8 @@ int main(void)
 		if (k == 81)
 		{
 			printf("Proper sudoku matrix %d: \n", ++num);
-			sudoku_print(sudoku);
-			if (num >= SUDOKU_NUM)
+		//	printSudoku(sudoku);
+			if (num >= N)
 				return 0;
 
 			--k;
@@ -152,4 +65,41 @@ int main(void)
 	}
 	system("pause");
 	return 0;
+}
+
+
+
+bool check(int sudoku[][9], int i, int j)
+{
+	int value = sudoku[i][j];
+	int p, q;
+	int m, n;
+
+	for (p = 0; p<9; p++)
+		if (p != i && sudoku[p][j] == value)
+			return false;
+	for (p = 0; p<9; p++)
+		if (p != j && sudoku[i][p] == value)
+			return false;
+
+	p = i / 3;
+	q = j / 3;
+	for (m = p * 3; m<p * 3 + 3; m++)
+		for (n = q * 3; n<q * 3 + 3; n++)
+			if (m != i && n != j && sudoku[m][n] == value)
+				return false;
+
+	return true;
+}
+
+
+void printSudoku(int sudoku[][9])
+{
+	int i, j;
+	for (i = 0; i<9; i++)
+	{
+		for (j = 0; j<9; j++)
+			printf("%4d", sudoku[i][j]);
+		printf("\n");
+	}
 }
